a56368cbd45372915eb779e04677457e3fe8dfe7
mammothb <mammothb+git@gmail.com>
2015-11-02 19:33:25 +0800

    added reference to parameters used

--- Parameters.m
+++ Parameters.m
@@ -1,5 +1,9 @@
 %===============================================================================
 % Creates an object with the general parameters of the model
+% Parameters from the following paper:
+% 1: Impact of the Fahraeus Effect on NO and O2 Biotransport A Computer Model
+% 2: Two-dimensional transient model for prediction of arteriolar NO/O2
+%    modulation by spatiotemporal variations in cell-free layer width
 %===============================================================================
 function obj = Parameters()
   obj.alpha = 1.3;  % [uM/Torr], Solubility
@@ -15,5 +19,3 @@ function obj = Parameters()
   obj.wss = 1.5;  % [Pa], Wall shear stress
   obj.wss_ref = 2.4;  % [Pa], Reference wall shear stress
 end
-% Two-dimensional transient model for prediction of arteriolar NO/O2 modulation
-% by spatiotemporal variations in cell-free layer width
