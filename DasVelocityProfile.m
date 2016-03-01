5d9093e4c8c00b4da57b5171878c4b6ec9e2d6b7
mammothb <mammothb+git@gmail.com>
2016-03-01 14:28:57 +0800

    added code to compute various parameters based on shear rate value computed from implicit expression for debugging purposes

--- DasVelocityProfile.m
+++ DasVelocityProfile.m
@@ -6,7 +6,7 @@ n = 20;  % Power law index
 H_min = 0.0;  % Minimum hematocrit
 H_t = 0.25;  % Tube hematorcrit
 % Arbitrary
-R1 = 25.0;  % Vessel radius
+R1 = 20.0;  % Vessel radius
 R2 = 0.7 * R1;  % RBC core radius
 lambda = R2 / R1;  % Core thickness
 h = 0.2 * R1;  % Minimum gap width
@@ -51,7 +51,7 @@ end
 
 % Parameters for RK4
 fmin = 0;
-fmax = 5000;
+fmax = 1e6;
 tau_wm = 1.4;  % dyne / cm^2, Characteristic wall shear stress
 mu_p = 1.3;
 % P_g = 2 * tau_wm * 1e-5 * 1e8 / R1;
@@ -75,6 +75,44 @@ tau_0 = mu_p .* gamma_c .* (0.5 .* H .* (k_0 - k_inf)) .^ 2 ./ (1 - 0.5 .*...
 q = (sqrt(tau_0) - sqrt(mu_inf .* Lambda)) ./ (sqrt(tau_0) + sqrt(mu_inf .*...
     Lambda));
 % C_r = sqrt(r - 2 .* alpha .* q .* sqrt(r) + alpha .^ 2);
+
+% Allocate memory
+k = zeros(nn, nn);
+mu = zeros(nn, nn);
+tau_w = zeros(nn, nn);
+alpha = zeros(nn, nn);
+gamma_dot = zeros(nn, nn);
+for ii = 1 : nn
+  for jj = 1 : nn
+    k_0_i = k_0(ii, jj);
+    k_inf_i = k_inf(ii, jj);
+    gamma_c_i = gamma_c(ii, jj);
+    H_i = H(ii, jj);
+    tau_0_i = tau_0(ii, jj);
+    mu_inf_i = mu_inf(ii, jj);
+    Lambda_i = Lambda(ii, jj);
+    q_i = q(ii, jj);
+    r_i = r(ii, jj);
+    k_i = @(I_e) (k_0_i + k_inf_i * sqrt(I_e / gamma_c_i)) / (1 + sqrt(I_e /...
+        gamma_c_i));  % *
+    mu_i = @(I_e) mu_p / (1 - 0.5 * k_i(I_e) * H_i) ^ 2 / mu_inf_m;  % *
+    tau_w_i = @(I_e) 8 * mu_i(I_e) * gamma_s;  % *
+    alpha_i = @(I_e) (sqrt(tau_0_i) + sqrt(mu_inf_i * Lambda_i)) /...
+        sqrt(lambda * tau_w_i(I_e));
+    C_r_i = @(I_e) sqrt(r_i - 2 * alpha_i(I_e) * q_i * sqrt(r_i) +...
+        alpha_i(I_e) ^ 2);
+    dvdr = @(I_e) P_g * R1 / 4 / mu_inf_i * (r_i - alpha_i(I_e) *...
+        (1 + q_i) * sqrt(r_i) + alpha_i(I_e)^2 + (sqrt(r_i) - alpha_i(I_e)) *...
+        C_r_i(I_e)) - I_e;
+    sr = fzero(dvdr, [0 100]);
+    k(ii, jj) = k_i(sr);
+    mu(ii, jj) = mu_i(sr);
+    tau_w(ii, jj) = tau_w_i(sr);
+    alpha(ii, jj) = alpha_i(sr);
+    gamma_dot(ii, jj) = sr;
+  end
+end
+C_r = sqrt(r - 2 .* alpha .* q .* sqrt(r) + alpha .^ 2);
 % Arbitrary
 h = 0.02;
 R = 0 : h : 1;
@@ -97,58 +135,100 @@ v = zeros(1, numel(R));
 %   k4 = h * dvdr(R(ii + incr) + h, v(ii + incr) + k3);
 %   v(ii) = v(ii + incr) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
 % end
-for ii = numel(R) - 1 : -1 : 1
-  x_i = 51;
-  z_i = x_i + ii;
-  % Model Parameters
-  k_0_i = k_0(z_i, x_i);
-  k_inf_i = k_inf(z_i, x_i);
-  gamma_c_i = gamma_c(z_i, x_i);
-  H_i = H(z_i, x_i);
-  tau_0_i = tau_0(z_i, x_i);
-  mu_inf_i = mu_inf(z_i, x_i);
-  Lambda_i = Lambda(z_i, x_i);
-  q_i = q(z_i, x_i);
-  r_i = r(z_i, x_i);
-  k = @(I_e) (k_0_i + k_inf_i * sqrt(I_e / gamma_c_i)) / (1 + sqrt(I_e /...
-      gamma_c_i));  % *
-  mu = @(I_e) mu_p / (1 - 0.5 * k(I_e) * H_i) ^ 2 / mu_inf_m;  % *
-  tau_w = @(I_e) 8 * mu(I_e) * gamma_s;  % *
-  alpha_i = @(I_e) (sqrt(tau_0_i) + sqrt(mu_inf_i * Lambda_i)) /...
-      sqrt(lambda * tau_w(I_e));
-  C_r_i = @(I_e) sqrt(r_i - 2 * alpha_i(I_e) * q_i * sqrt(r_i) +...
-      alpha_i(I_e) ^ 2);
-  % Reverse sign when going from R1 to 0
-  r_i = r(z_i, x_i);
-  dvdr = @(r_i, x) P_g * R1 / 4 / mu_inf_i * (r_i - alpha_i * (1 + q_i) *...
-      sqrt(r_i) + alpha_i^2 + (sqrt(r_i) - alpha_i) * C_r_i);
-  r1 = R(ii + incr);
-  r2 = R(ii + incr) + h / 2;
-  r3 = R(ii + incr) + h / 2;
-  r4 = R(ii + incr) + h;
-  dvdr1 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r1 - alpha_i(I_e) * (1 + q_i) *...
-      sqrt(r1) + alpha_i(I_e)^2 + (sqrt(r1) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
-  dvdr2 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r2 - alpha_i(I_e) * (1 + q_i) *...
-      sqrt(r2) + alpha_i(I_e)^2 + (sqrt(r2) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
-  dvdr3 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r3 - alpha_i(I_e) * (1 + q_i) *...
-      sqrt(r3) + alpha_i(I_e)^2 + (sqrt(r3) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
-  dvdr4 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r4 - alpha_i(I_e) * (1 + q_i) *...
-      sqrt(r4) + alpha_i(I_e)^2 + (sqrt(r4) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
-  k1 = h * fzero(dvdr1, [fmin fmax]);
-  k2 = h * fzero(dvdr2, [fmin fmax]);
-  k3 = h * fzero(dvdr3, [fmin fmax]);
-  k4 = h * fzero(dvdr4, [fmin fmax]);
-  % k1 = h * dvdr(R(ii + incr), v(ii + incr));
-  % k2 = h * dvdr(R(ii + incr) + h / 2, v(ii + incr) + k1 / 2);
-  % k3 = h * dvdr(R(ii + incr) + h / 2, v(ii + incr) + k2 / 2);
-  % k4 = h * dvdr(R(ii + incr) + h, v(ii + incr) + k3);
-  v(ii) = v(ii + incr) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-end
+% for ii = numel(R) - 1 : -1 : 1
+%   x_i = 51;
+%   z_i = x_i + ii;
+%   % Model Parameters
+%   k_0_i = k_0(z_i, x_i);
+%   k_inf_i = k_inf(z_i, x_i);
+%   gamma_c_i = gamma_c(z_i, x_i);
+%   H_i = H(z_i, x_i);
+%   tau_0_i = tau_0(z_i, x_i);
+%   mu_inf_i = mu_inf(z_i, x_i);
+%   Lambda_i = Lambda(z_i, x_i);
+%   q_i = q(z_i, x_i);
+%   r_i = r(z_i, x_i);
+%   k = @(I_e) (k_0_i + k_inf_i * sqrt(I_e / gamma_c_i)) / (1 + sqrt(I_e /...
+%       gamma_c_i));  % *
+%   mu = @(I_e) mu_p / (1 - 0.5 * k(I_e) * H_i) ^ 2 / mu_inf_m;  % *
+%   tau_w = @(I_e) 8 * mu(I_e) * gamma_s;  % *
+%   alpha_i = @(I_e) (sqrt(tau_0_i) + sqrt(mu_inf_i * Lambda_i)) /...
+%       sqrt(lambda * tau_w(I_e));
+%   C_r_i = @(I_e) sqrt(r_i - 2 * alpha_i(I_e) * q_i * sqrt(r_i) +...
+%       alpha_i(I_e) ^ 2);
+%   % Reverse sign when going from R1 to 0
+%   r_i = r(z_i, x_i);
+%   dvdr = @(r_i, x) P_g * R1 / 4 / mu_inf_i * (r_i - alpha_i * (1 + q_i) *...
+%       sqrt(r_i) + alpha_i^2 + (sqrt(r_i) - alpha_i) * C_r_i);
+%   r1 = R(ii + incr);
+%   r2 = R(ii + incr) + h / 2;
+%   r3 = R(ii + incr) + h / 2;
+%   r4 = R(ii + incr) + h;
+%   dvdr1 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r1 - alpha_i(I_e) * (1 + q_i) *...
+%       sqrt(r1) + alpha_i(I_e)^2 + (sqrt(r1) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
+%   dvdr2 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r2 - alpha_i(I_e) * (1 + q_i) *...
+%       sqrt(r2) + alpha_i(I_e)^2 + (sqrt(r2) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
+%   dvdr3 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r3 - alpha_i(I_e) * (1 + q_i) *...
+%       sqrt(r3) + alpha_i(I_e)^2 + (sqrt(r3) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
+%   dvdr4 = @(I_e) P_g * R1 / 4 / mu_inf_i * (r4 - alpha_i(I_e) * (1 + q_i) *...
+%       sqrt(r4) + alpha_i(I_e)^2 + (sqrt(r4) - alpha_i(I_e)) * C_r_i(I_e)) - I_e;
+%   k1 = h * fzero(dvdr1, [fmin fmax]);
+%   k2 = h * fzero(dvdr2, [fmin fmax]);
+%   k3 = h * fzero(dvdr3, [fmin fmax]);
+%   k4 = h * fzero(dvdr4, [fmin fmax]);
+%   % k1 = h * dvdr(R(ii + incr), v(ii + incr));
+%   % k2 = h * dvdr(R(ii + incr) + h / 2, v(ii + incr) + k1 / 2);
+%   % k3 = h * dvdr(R(ii + incr) + h / 2, v(ii + incr) + k2 / 2);
+%   % k4 = h * dvdr(R(ii + incr) + h, v(ii + incr) + k3);
+%   v(ii) = v(ii + incr) + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+% end
 
-plot(R, v * tau_wm * R1 / 2 / mu_inf_m);
-
-% mesh(x, z, H);
-% xlabel('x');
-% ylabel('z');
-% zlabel('H');
-% zlim([0 0.8]);
+% plot(R, v * tau_wm * R1 / 2 / mu_inf_m);
+plot_y = 4;
+plot_x = 4;
+subplot(plot_y, plot_x, 1);
+mesh(x, z, H);
+xlabel('x');
+ylabel('z');
+zlabel('H');
+zlim([0 0.8]);
+title('H');
+subplot(plot_y, plot_x, 2);
+mesh(x, z, k_0);
+title('k_0');
+subplot(plot_y, plot_x, 3);
+mesh(x, z, k_inf);
+title('k_\infty');
+subplot(plot_y, plot_x, 4);
+mesh(x, z, gamma_c);
+title('\gamma_c');
+subplot(plot_y, plot_x, 5);
+mesh(x, z, mu_inf);
+title('\mu_\infty');
+subplot(plot_y, plot_x, 6);
+mesh(x, z, Lambda);
+title('\Lambda');
+subplot(plot_y, plot_x, 7);
+mesh(x, z, tau_0);
+title('\tau_0');
+subplot(plot_y, plot_x, 8);
+mesh(x, z, alpha);
+title('\alpha *');
+subplot(plot_y, plot_x, 9);
+mesh(x, z, q);
+title('q');
+subplot(plot_y, plot_x, 10);
+mesh(x, z, C_r);
+title('C_r');
+subplot(plot_y, plot_x, 11);
+mesh(x, z, k);
+title('k');
+subplot(plot_y, plot_x, 12);
+mesh(x, z, mu);
+title('\mu');
+subplot(plot_y, plot_x, 13);
+mesh(x, z, tau_w);
+title('\tau_w');
+subplot(plot_y, plot_x, 14);
+mesh(x, z, gamma_dot);
+title('\gamma');
